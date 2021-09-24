6c9ec13df1b16e9dbfc58fe39cee9fa6cd2b48d8
sh <shwan@f1security.co.kr>
2021-09-24 10:56:12 +0900

    feat: writing result datas

--- examples/example.py
+++ examples/example.py
@@ -8,7 +8,7 @@ from eva.metric import valuation_mse
 from random import uniform
 import numpy as np
 
-poly = EvaProgram('Polynomial', vec_size=(10**5))
+poly = EvaProgram("Polynomial", vec_size=(10**5))
 with poly:
     a = Input("a")
     b = Input("b")
@@ -23,8 +23,8 @@ poly, params, signature = compiler.compile(poly)
 public_ctx, secret_ctx = generate_keys(params)
 
 inputs = {
-    'a': [uniform(1000, 10000) for _ in range(signature.vec_size)],
-    'b': [uniform(0.001, 0.01) for _ in range(signature.vec_size)]
+    "a": [uniform(1000, 10000) for _ in range(signature.vec_size)],
+    "b": [uniform(0.001, 0.01) for _ in range(signature.vec_size)]
 }
 encInputs = public_ctx.encrypt(inputs, signature)
 
@@ -41,9 +41,35 @@ got = outputs["result"]
 error_rate = []
 for i in range(signature.vec_size):
     error_rate.append((abs(expected[i]-got[i])/abs(expected[i]))*100)
+error_rates = {}
+error_rates["result"] = error_rate
 
 print("inputs", inputs)
-print('Expected', reference)
-print('Got', outputs)
-print("Error Rate", error_rate)
-print('MSE', valuation_mse(outputs, reference))
+print("Origin outputs", reference)
+print("Encrypted outputs", outputs)
+print("Error Rates", error_rates)
+#print("MSE", valuation_mse(outputs, reference))
+
+file = open("ckks_mult_example.txt", "w")
+for i in range(signature.vec_size):
+    if error_rates["result"][i] < 0.0001:
+        result = "True"
+        data = "\"a^5\": " + str(inputs["a"][i]) + " " \
+               + "\"b^4\": " + str(inputs["b"][i]) + " " \
+               + "\"Origin output\": " + str(reference["result"][i]) + " " \
+               + "\"Encrypted output\": " + str(outputs["result"][i]) + " " \
+               + "\"Error Rate\": " + str(result)
+
+        file.write(data + "\n")
+    else:
+        result = "False"
+        data = "\"a^5\": " + str(inputs["a"][i]) + " " \
+               + "\"b^4\": " + str(inputs["b"][i]) + " " \
+               + "\"Origin output\": " + str(reference["result"][i]) + " " \
+               + "\"Encrypted output\": " + str(outputs["result"][i]) + " " \
+               + "\"Error Rate\": " + str(result)
+
+        file.write(data + "\n")
+
+file.close()
+print("Complete")
